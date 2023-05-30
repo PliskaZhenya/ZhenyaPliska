b8648d61d37d49ec6efb2295dd1ca3ba6a8c8e99
Zhenya <pliskaz1010@gmail.com>
2023-05-30 16:50:34 +0300

    some msg

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
-let seconds = prompt("input seconds:");
-const min = 60;
-const hour = min * 60;
-const day = hour * 24;
-let days = Math.floor(seconds/day);
-let reminder = seconds % day;
-let hours = Math.floor(reminder/hour);
-reminder = reminder % hour;
-let mins = Math.floor(reminder/min);
-reminder = reminder % min;
-console.log(String(days).padStart(2, "0") + ":" + String(hours).padStart(2, "0") + ":" + String(mins).padStart(2, "0") + ":" + String(reminder).padStart(2, "0"));
+const age = prompt("Введите ваш возраст");
+if (age > 100) {
+    console.log("Вы все врете");
+}  else if (age > 18) {
+console.log("Everything is fine");
+}  else  {
+    console.log("everything is bad");
+} 
+
+
+
 
