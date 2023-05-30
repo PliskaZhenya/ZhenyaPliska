2ddab8887836b493560e470dad52a016277b9515
Zhenya <pliskaz1010@gmail.com>
2023-05-30 19:44:11 +0300

    some msg

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
 const age = prompt("Введите ваш возраст");
 if (age > 100) {
     console.log("Вы все врете");
-}  else if (age > 18) {
+   }  else if (age > 18) {
 console.log("Everything is fine");
-}  else  {
+   } else {
     console.log("everything is bad");
-} 
+   } 
+if (age % 2 ===0)  {
+console.log("четное");
+} else {
+    console.log("нечетное");
+}
+    
 
 
 
